9a0169bdc2b44561b9f93abf2b86003b9da262c0
Aratikadam <95737222+Aratikadam@users.noreply.github.com>
2024-08-21 22:06:16 -0700

    Add files via upload
    Due to exception the remaining code after that exception is not executed.

---
+++ Dividezero.java
@@ -0,0 +1,11 @@
+class Dividezero
+{
+    public static void main(String args[])
+    {
+        System.out.println("Program is started");
+        int a=10,b=0,c;
+        c=a/b;
+        System.out.println(c);
+        System.out.println("Program is ended");
+    }
+}
